4dd97c3986920ff32af2d53b1ced140c307377dc
MagicSalt <darrenlin815@hotmail.com>
2023-07-01 15:05:29 -0700

    Adds some styling to the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
 export default function Footer({ name, email, phone }) {
     return (
         <>
-            <footer>
+            <footer className='footer border-t-2 border-gray-300 pt-5'>
                 <ul className='flex flex-wrap items-center justify-center'>
                     <li>
-                        <span className='font-bold'>Your Name: </span>
+                        <span className='font-bold'>Your Name:</span>
                         {name}
                     </li>
                     <li>
-                        <span className='font-bold'>Your Email: </span>
+                        <span className='font-bold'>Your Email:</span>
                         {email}
                     </li>
                     <li>
-                        <span className='font-bold'>Phone Number: </span>
+                        <span className='font-bold'>Phone Number:</span>
                         {phone}
                     </li>
                 </ul>
