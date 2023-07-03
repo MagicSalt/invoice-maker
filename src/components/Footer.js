abf8d336cbc66ba1795cab512110469c2a962324
MagicSalt <darrenlin815@hotmail.com>
2023-07-03 15:09:56 -0700

    Adds some styling to the input form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ export default function Footer({ name, email, phone }) {
             <footer className='footer border-t-2 border-gray-300 pt-5'>
                 <ul className='flex flex-wrap items-center justify-center'>
                     <li>
-                        <span className='font-bold'>Your Name:</span>
+                        <span className='font-bold'>Your Name: </span>
                         {name}
                     </li>
                     <li>
-                        <span className='font-bold'>Your Email:</span>
+                        <span className='font-bold'>Your Email: </span>
                         {email}
                     </li>
                     <li>
-                        <span className='font-bold'>Phone Number:</span>
+                        <span className='font-bold'>Phone Number: </span>
                         {phone}
                     </li>
                 </ul>
