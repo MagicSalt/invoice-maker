e3e9516d1adcce7c44b9b144471728dcfe564757
MagicSalt <darrenlin815@hotmail.com>
2023-07-03 16:21:44 -0700

    Adds a basic table element to the invoice

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,25 @@
 export default function Table() {
     return (
         <>
-            <div className='my-5'>this is the table</div>
+            <table width='100%'>
+                <thead>
+                    <tr className='bg-gray-100 p-1'>
+                        <td>Description</td>
+                        <td>Quantity</td>
+                        <td>Price</td>
+                        <td>Amount</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Sample description</td>
+                        <td>20</td>
+                        <td>500</td>
+                        <td>10000</td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     );
 }
